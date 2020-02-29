faa5274366d998eff173bb6b20ff06d24ab27a15
Abel Tran <ampup.tott@gmail.com>
2020-03-01 00:27:35 +0700

    connect to database

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@
 
 const app = require('router')();
 const body = require('body-parser');
+require('../mongoosefile');
 
 app.use(body.json({ limit: '50mb' }));
 app.use(require('./mid/query'));
 app.use(require('./mid/json'));
+
 
 app.get('/', require('./lambda/status'));
 
