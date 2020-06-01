5b5c68e90a341db21b5337bbae528eb9efeb6cbd
Abel Tran <ampup.tott@gmail.com>
2020-06-01 18:35:45 +0700

    add mask

--- src/lambda/subject/add-subject.js
+++ src/lambda/subject/add-subject.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 
 module.exports = async (req, res, next) => {
   try {
-    const { id, name } = req.body;
+    const { id, name, mask } = req.body;
 
     if (!id) {
       return next('Missing parameter: id');
@@ -14,25 +14,31 @@ module.exports = async (req, res, next) => {
     if (!name) {
       return next('Missing parameter: name');
     }
-    
+
+    if (!mask) {
+      return next('Missing parameter: mask');
+    }
+
     const subject = await Subject.findOne({ id });
     if (subject) {
       return next('Subject is existed');
     }
 
     const utc_now = moment().unix();
-    await Subject.insertMany([{
-      id,
-      name,
-      created_at: utc_now,
-      updated_at: utc_now
-    }]);
+    await Subject.insertMany([
+      {
+        id,
+        name,
+        mask,
+        created_at: utc_now,
+        updated_at: utc_now,
+      },
+    ]);
 
     return res.json({
-      status: 'OK'
+      status: 'OK',
     });
-
-  } catch(error) {
+  } catch (error) {
     return next(error.message);
-  };
+  }
 };
--- src/models/Subject.js
+++ src/models/Subject.js
@@ -5,9 +5,10 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   id: { type: String, trim: true },
   name: { type: String, trim: true },
+  mask: { type: String, trim: true },
   status: { type: Boolean, default: true },
   created_at: { type: Number },
-  updated_at: { type: Number }
+  updated_at: { type: Number },
 });
 
 module.exports = mongoose.model('Subject', schema, 'subject');
