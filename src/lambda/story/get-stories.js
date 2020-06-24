d8cec3517a9b3aa55ce505a6a863dc4a6312a2df
Abel Tran <ampup.tott@gmail.com>
2020-06-24 23:46:31 +0700

    add field for stories

--- src/lambda/story/get-stories.js
+++ src/lambda/story/get-stories.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Story from '../../models/Story';
+import Student from '../../models/Student';
 
 module.exports = async (req, res, next) => {
   const { class_id } = req.params;
@@ -14,11 +15,41 @@ module.exports = async (req, res, next) => {
     return next('Missing parameter: date');
   }
 
-  const stories = await Story.find({ class_id, date });
+  let stories = await Story.find({ class_id, date }).select([
+    'student_id',
+    'class_id',
+    'date',
+  ]);
 
-  if (!stories) {
+  if (!stories.length) {
     return next('Wrong parameter: class_id');
   }
+
+  let student_ids = stories.map((story) => story.student_id);
+
+  const students = await Student.find({
+    _id: {
+      $in: student_ids,
+    },
+  }).select(['name', 'id', 'phone', 'address', 'email']);
+
+  if (!students.length) {
+    return next('Something went wrong for class student');
+  }
+
+  stories = stories.map((story) => {
+    const student = students.find((student) => student._id == story.student_id);
+    return {
+      student_id: story.student_id,
+      class_id: story.class_id,
+      date: story.date,
+      name: student.name,
+      id: student.id,
+      phone: student.phone,
+      address: student.address,
+      email: student.email,
+    };
+  });
 
   return res.json({
     status: 'OK',
